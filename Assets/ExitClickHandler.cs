25b33f445b5f02b54f6a078629a6f1b66d3516a4
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-19 22:24:48 +0700

    Add files via upload

---
+++ Assets/ExitClickHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitClickHandler : MonoBehaviour
+{
+    public void doquit()
+    {
+        Application.Quit();
+        Debug.Log("Successfully Exited the Game");
+    }
+}
